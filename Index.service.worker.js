3b6b6e60dfe43f2696d8078cb1688f8d7c1f8240
Spencer Chang <swifthider1234@gmail.com>
2025-03-18 01:13:13 -0700

    Add story button for campaigns

--- Index.service.worker.js
+++ Index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742278093|39621268234';
+const CACHE_VERSION = '1742285576|47103768384';
 /** @type {string} */
 const CACHE_PREFIX = 'ProjectGH-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
