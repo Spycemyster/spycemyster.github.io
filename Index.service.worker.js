e5121f909a2b9f6728d63a8abe41cac748161563
Spencer Chang <swifthider1234@gmail.com>
2025-03-17 23:02:37 -0700

    Add Harem Page

--- Index.service.worker.js
+++ Index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741058250|8961450927';
+const CACHE_VERSION = '1742277732|39260180095';
 /** @type {string} */
 const CACHE_PREFIX = 'ProjectGH-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
